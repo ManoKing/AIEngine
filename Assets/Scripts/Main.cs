1b8341934725599932178d27fb817f7bb175fff0
冯云龙 <fengyunlong@galasports.com>
2020-07-30 18:11:09 +0800

    Add scene manager

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        SceneManager.CreateInstance(gameObject);
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Module/UILogin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UILogin : MonoBehaviour
+{
+    public Button login;
+    void Start()
+    {
+        login.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene("Home");
+        });
+    }
+}
