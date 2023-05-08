b60b621290f47419701c27ddb633676e38971971
DESKTOP-6Q27J9E\admin <fengyunlong@tuyoogame.com>
2023-05-08 14:45:37 +0800

    添加新工程

--- AIEngine/Assets/Engine/Edite/AITestEdit.cs
+++ AIEngine/Assets/Engine/Edite/AITestEdit.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AITest : MonoBehaviour
+public class AITestEdit : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
